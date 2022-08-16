b5e7aec918d8c5eee1c197361f9cd554535a7c40
HUIKIM0 <fehs123@naver.com>
2022-08-16 22:59:43 +0900

    Initial commit

---
+++ Test_Parallel/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace Test_Parallel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnfor = new System.Windows.Forms.Button();
+            this.btnParallelFor = new System.Windows.Forms.Button();
+            this.btnParallelForeach = new System.Windows.Forms.Button();
+            this.btnParallelInvoke = new System.Windows.Forms.Button();
+            this.lboxTime = new System.Windows.Forms.ListBox();
+            this.lboxLog = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Controls.Add(this.lboxLog);
+            this.groupBox1.Controls.Add(this.lboxTime);
+            this.groupBox1.Controls.Add(this.btnParallelInvoke);
+            this.groupBox1.Controls.Add(this.btnParallelForeach);
+            this.groupBox1.Controls.Add(this.btnParallelFor);
+            this.groupBox1.Controls.Add(this.btnfor);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(599, 505);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "1. Parallel";
+            // 
+            // btnfor
+            // 
+            this.btnfor.Location = new System.Drawing.Point(7, 25);
+            this.btnfor.Name = "btnfor";
+            this.btnfor.Size = new System.Drawing.Size(140, 30);
+            this.btnfor.TabIndex = 0;
+            this.btnfor.Text = "for";
+            this.btnfor.UseVisualStyleBackColor = true;
+            this.btnfor.Click += new System.EventHandler(this.btnfor_Click);
+            // 
+            // btnParallelFor
+            // 
+            this.btnParallelFor.Location = new System.Drawing.Point(153, 25);
+            this.btnParallelFor.Name = "btnParallelFor";
+            this.btnParallelFor.Size = new System.Drawing.Size(140, 30);
+            this.btnParallelFor.TabIndex = 1;
+            this.btnParallelFor.Text = "Parallel For";
+            this.btnParallelFor.UseVisualStyleBackColor = true;
+            this.btnParallelFor.Click += new System.EventHandler(this.btnParallelFor_Click);
+            // 
+            // btnParallelForeach
+            // 
+            this.btnParallelForeach.Location = new System.Drawing.Point(299, 25);
+            this.btnParallelForeach.Name = "btnParallelForeach";
+            this.btnParallelForeach.Size = new System.Drawing.Size(140, 30);
+            this.btnParallelForeach.TabIndex = 2;
+            this.btnParallelForeach.Text = "Parallel Foreach";
+            this.btnParallelForeach.UseVisualStyleBackColor = true;
+            this.btnParallelForeach.Click += new System.EventHandler(this.btnParallelForeach_Click);
+            // 
+            // btnParallelInvoke
+            // 
+            this.btnParallelInvoke.Location = new System.Drawing.Point(445, 25);
+            this.btnParallelInvoke.Name = "btnParallelInvoke";
+            this.btnParallelInvoke.Size = new System.Drawing.Size(140, 30);
+            this.btnParallelInvoke.TabIndex = 3;
+            this.btnParallelInvoke.Text = "Parallel Invoke";
+            this.btnParallelInvoke.UseVisualStyleBackColor = true;
+            this.btnParallelInvoke.Click += new System.EventHandler(this.btnParallelInvoke_Click);
+            // 
+            // lboxTime
+            // 
+            this.lboxTime.FormattingEnabled = true;
+            this.lboxTime.ItemHeight = 15;
+            this.lboxTime.Location = new System.Drawing.Point(7, 62);
+            this.lboxTime.Name = "lboxTime";
+            this.lboxTime.Size = new System.Drawing.Size(578, 169);
+            this.lboxTime.TabIndex = 4;
+            // 
+            // lboxLog
+            // 
+            this.lboxLog.FormattingEnabled = true;
+            this.lboxLog.ItemHeight = 15;
+            this.lboxLog.Location = new System.Drawing.Point(6, 238);
+            this.lboxLog.Name = "lboxLog";
+            this.lboxLog.Size = new System.Drawing.Size(579, 259);
+            this.lboxLog.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 529);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnParallelInvoke;
+        private System.Windows.Forms.Button btnParallelForeach;
+        private System.Windows.Forms.Button btnParallelFor;
+        private System.Windows.Forms.Button btnfor;
+        private System.Windows.Forms.ListBox lboxLog;
+        private System.Windows.Forms.ListBox lboxTime;
+    }
+}
+
---
+++ Test_Parallel/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_Parallel
+{
+    public partial class Form1 : Form
+    {
+        enum enCase
+        {
+            nfor,
+            pFor,
+            pForeach,
+            pInvoke,
+        }
+
+        #region
+        private int _iThreadSleepTime = 10;
+
+        private int _iforStart = 0;
+        private int _iforCount = 100;
+
+        DateTime startTime;  // 작업 시작 시각
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnfor_Click(object sender, EventArgs e)
+        {
+            pThread(enCase.nfor);
+        }
+
+        private void btnParallelFor_Click(object sender, EventArgs e)
+        {
+            pThread(enCase.pFor);
+        }
+
+        private void btnParallelForeach_Click(object sender, EventArgs e)
+        {
+            pThread(enCase.pForeach);
+        }
+
+        private void btnParallelInvoke_Click(object sender, EventArgs e)
+        {
+            pThread(enCase.pInvoke);
+        }
+
+        
+        // Thread
+        private void pThread(enCase eCase)
+        {
+            Log(lboxTime, enLogLevel.Info, string.Format("Thread Start"));
+            startTime = DateTime.Now;  
+
+            Thread _thread;
+
+            switch (eCase)
+            {
+                case enCase.nfor:
+                    _thread = new Thread(Tnfor);
+                    _thread.Start();
+                    break;
+
+                case enCase.pFor:
+                    _thread = new Thread(TpFor);
+                    _thread.Start();
+                    break;
+                case enCase.pForeach:
+                    _thread = new Thread(TpForeach);
+                    _thread.Start();
+                    break;
+                case enCase.pInvoke:
+                    _thread = new Thread(TpInvoke);
+                    _thread.Start();
+                    break;
+            }
+        }
+
+
+        #region 분산처리 Ex
+        // 그냥 for문(분산처리 아님. 순차대로 처리됨)
+        private void Tnfor()
+        {
+            for (int i = _iforStart; i < _iforCount; i++)
+            {
+                Log(enLogLevel.Info, $"for: {i}");
+                Thread.Sleep(_iThreadSleepTime);
+            }
+
+            DateTime endTime = DateTime.Now;
+            Log(lboxTime, enLogLevel.Info, $"Thread End : {endTime - startTime}");
+        }
+
+        // Parallel For
+        private void TpFor()
+        {
+            int iTotal = 0;
+
+            Parallel.For(_iforStart, _iforCount, i =>      
+            {
+                Log(enLogLevel.Info, $"Parallel For : {i}");
+                Thread.Sleep(_iThreadSleepTime);
+            });
+
+            DateTime endTime = DateTime.Now;
+            Log(lboxTime, enLogLevel.Info,$"Thread End : {endTime - startTime}");
+
+            /*
+              Parallel.For(_iforStart, _iforCount, pfun);  
+
+              Action<int> afun = pfun;
+
+              public static void pfun(int a)
+              {
+
+              }
+            */
+
+            // 무명메서드
+            /*
+                Parallel.For(_iforStart, _iforCount, delegate(int i)
+                {
+                    Log(enLogLevel.Info, $"pfor(무명) : {i}");
+                    Thread.Sleep(10);
+                });
+                */
+
+            /*
+                // 람다식
+                Parallel.For(_iforStart, _iforCount, (i) =>
+                {
+                    Log(enLogLevel.Info, $"pfor(람다) : {i}");
+                    Thread.Sleep(10);
+                });
+                */
+        }
+
+        // Parallel Foreach
+        private void TpForeach()
+        {
+            /* ForEach는 List형태 같은걸 빼오니까 List생성 */
+
+            //List<int> iList = (Enumerable.Repeat(3, 5)).ToList<int>();   // 초기화를 3,3,3,3,3
+            List<int> iList = (Enumerable.Range(_iforStart, _iforCount)).ToList<int>();   // 선언 하면서 순차적으로 값 넣기
+
+            Parallel.ForEach(iList, i =>
+            {
+                Log(enLogLevel.Info, $"Parallel Foreach : {i}");
+                Thread.Sleep(_iThreadSleepTime);
+            });
+
+            DateTime endTime = DateTime.Now;
+            Log(lboxTime, enLogLevel.Info, $"Thread End : {endTime - startTime}");
+        }
+
+        // Parallel Invoke(Function
+        private void TpInvoke()
+        {
+            /* TpFor 함수와 TpForeach 함수 함께 호출한다고 보면 된다 */
+
+            Parallel.Invoke(
+                TpFor,
+                TpForeach
+            );
+        }
+        #endregion
+
+
+        #region Log Viewer 
+        enum enLogLevel
+        {
+            Info,
+            Warning,
+            Error,
+        }
+
+        private void Log(enLogLevel eLevel, string LogDesc)
+        {
+            this.Invoke(new Action(delegate ()
+            {
+                DateTime dTime = DateTime.Now;
+                string LogInfo = $"{dTime:yyyy-MM-dd hh:mm:ss.fff} [{eLevel.ToString()}] {LogDesc}";
+                lboxLog.Items.Insert(0, LogInfo);
+                Refresh();
+            }));
+        }
+
+        private void Log(DateTime dTime, enLogLevel eLevel, string LogDesc)
+        {
+            this.Invoke(new Action(delegate ()
+            {
+                string LogInfo = $"{dTime:yyyy-MM-dd hh:mm:ss.fff} [{eLevel.ToString()}] {LogDesc}";
+                lboxLog.Items.Insert(0, LogInfo);
+            }));
+        }
+
+        private void Log(ListBox lbox, enLogLevel eLevel, string LogDesc)
+        {
+            this.Invoke(new Action(delegate ()
+            {
+                DateTime dTime = DateTime.Now;
+                string LogInfo = $"{dTime:yyyy-MM-dd hh:mm:ss.fff} [{eLevel.ToString()}] {LogDesc}";
+                lbox.Items.Insert(0, LogInfo);
+            }));
+        }
+        #endregion
+    }
+}
